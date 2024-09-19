c2d85336f2c7977c102bc123d06cd49651b6d7b2
lollipopwyn <rwewine@gmail.com>
2024-09-19 10:26:51 +0900

    url경로 수정

--- src/util/apiUrl.js
+++ src/util/apiUrl.js
@@ -1,6 +1,7 @@
-const REGISTER_USER_API_URL = 'http://localhost:8080/register_user';
-const POST_TASKS_API_URL = 'http://localhost:8080/post_tasks';
-const GET_TASKS_API_URL = 'http://localhost:8080/get_tasks';
+// 회원
+const REGISTER_USER_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/register_user`;
+const POST_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/post_tasks`;
+const GET_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_tasks`;
 
 //Mypage 협의서 리스트
 const GET_USER_TASKS_API_URL = 'http://localhost:8080/get_UserTasks';
