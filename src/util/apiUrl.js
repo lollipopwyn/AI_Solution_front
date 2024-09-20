d25e3170c5566eeb815ba5d9af5eb2497a47bf37
mjk2024_dementia <minjukang20@gmail.com>
2024-09-20 10:50:23 +0900

    url를 env 파일로 수정

--- src/util/apiUrl.js
+++ src/util/apiUrl.js
@@ -1,28 +1,22 @@
 // 회원
-const REGISTER_USER_API_URL = 'https://solutionbackend.bstuz.com/register_user';
-const POST_TASKS_API_URL = 'https://solutionbackend.bstuz.com/post_tasks';
-const GET_TASKS_API_URL = 'https://solutionbackend.bstuz.com/get_tasks';
+const REGISTER_USER_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/register_user`;
+const POST_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/post_tasks`;
+const GET_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_tasks`;
 
 //Mypage 협의서 리스트
-const GET_USER_TASKS_API_URL =
-  'https://solutionbackend.bstuz.com/get_UserTasks';
+const GET_USER_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_UserTasks`;
 
-const POST_STATUS_TASKS_API_URL =
-  'https://solutionbackend.bstuz.com/post_status';
+const POST_STATUS_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/post_status`;
 
-const UPDADTE_AGREE_STATUS_API_URL =
-  'https://solutionbackend.bstuz.com/update_status';
-const UPDADTE_AGREE_STATUS_COMMENT_API_URL =
-  'https://solutionbackend.bstuz.com/update_comment_status';
-const DELETE_TASKS_API_URL = 'https://solutionbackend.bstuz.com/delete_task';
+const UPDADTE_AGREE_STATUS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_status`;
+const UPDADTE_AGREE_STATUS_COMMENT_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_comment_status`;
+const DELETE_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/delete_task`;
 
 //Mypage 협의서 수정
-const UPDATE_USER_AGREE_API_URL =
-  'https://solutionbackend.bstuz.com/update_agree';
+const UPDATE_USER_AGREE_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_agree`;
 
 //  패키지 카테고리 받아오기
-const GET_PACKAGES_CATEGORY_API_URL =
-  'https://solutionbackend.bstuz.com/get_Category/:text';
+const GET_PACKAGES_CATEGORY_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_Category/:text`;
 
 export {
   REGISTER_USER_API_URL,
